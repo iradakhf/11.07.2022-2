15b529d1f5150d59ae5874001031781da7fc52e0
iradakhf <iradakhf@code.edu.az>
2022-07-11 09:01:49 +0400

    add all

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace ConsoleApp29
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+            int[,] numbers = { { 1, 8, 5, 63 },{ 4,85,12,35} };
+            ReturnMultiplicationMethodOfGivenArray(numbers);
+        }
+        public static void ReturnMultiplicationMethodOfGivenArray(int[,] numbers)
+        {
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    if(i==numbers.GetLength(0)-1 && j == numbers.GetLength(1) - 1)
+                    {
+                    Console.Write($"{numbers[i,j]}");
+                    }
+
+                    else
+                    {
+                        Console.Write($"{numbers[i, j]} *");
+                    }
+
+                }
+            }
+            
+        }
+    }
+}
